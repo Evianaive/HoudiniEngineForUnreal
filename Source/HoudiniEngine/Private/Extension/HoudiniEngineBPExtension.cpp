c4bba972301a2cdc18a58d8de670c8b48d39f261
Evianaive <153540933@qq.com>
2024-03-03 23:07:26 +0800

    Init Blueprint Wrapper Class

---
+++ Source/HoudiniEngine/Private/Extension/HoudiniEngineBPExtension.cpp
@@ -0,0 +1,52 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HoudiniEngineBPExtension.h"
+
+#include "HoudiniEngine.h"
+
+ 
+FHoudiniNode::FHoudiniNode(int32 InNodeId)
+	:Session(FHoudiniEngine::Get().GetSession())
+	,NodeId(InNodeId)	
+{};
+
+void UHoudiniEngineBPExtension::SetArrayOfStructOnNodeInternal(const FScriptArray& InArrayOfStruct,
+	const FHoudiniNode& InNode, EAttributeOwner ImportLevel)
+{
+}
+
+void UHoudiniEngineBPExtension::GetArrayOfStructOnNodeInternal(FScriptArray& InArrayOfStruct,
+	const FHoudiniNode& InNode, EAttributeOwner ImportLevel)
+{
+}
+
+void UHoudiniEngineBPExtension::SetArrayOfStructOnNode_BP(
+	const TArray<int32>& InArrayOfStruct,
+	const FHoudiniNode& InNode,
+	EAttributeOwner ImportLevel)
+{
+	//We should never enter this
+}
+
+DEFINE_FUNCTION(UHoudiniEngineBPExtension::execSetArrayOfStructOnNode_BP)
+{
+	Stack.MostRecentProperty = nullptr;
+	Stack.StepCompiledIn<FArrayProperty>(NULL);
+	void* ArrayAddr = Stack.MostRecentPropertyAddress;
+	FArrayProperty* ArrayProperty = CastField<FArrayProperty>(Stack.MostRecentProperty);
+	if (!ArrayProperty)
+	{
+		Stack.bArrayContextFailed = true;
+		return;
+	}
+
+	P_GET_STRUCT_REF(FHoudiniNode,InNode);
+
+	P_FINISH;
+	P_NATIVE_BEGIN;
+	// MARK_PROPERTY_DIRTY(Stack.Object, ArrayProperty);
+	// GenericArray_Insert(ArrayAddr, ArrayProperty, StorageSpace, Index);
+	P_NATIVE_END;
+	// InnerProp->DestroyValue(StorageSpace);
+}
---
+++ Source/HoudiniEngine/Private/Extension/HoudiniEngineBPExtension.h
@@ -0,0 +1,70 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "UObject/Object.h"
+#include "HoudiniEngineBPExtension.generated.h"
+
+/**
+ * 
+ */
+UENUM()
+enum class EAttributeOwner : uint8
+{
+	Vertex = 0,
+	Point,
+	Prim,
+	Detail
+};
+
+
+// Force the user make an valid instance in blueprint
+USTRUCT(BlueprintType,meta=(HasNativeMake="/Script/HoudiniEngine.HoudiniEngineBPExtension.MakeHoudiniNode_BP"))
+struct FHoudiniNode
+{
+	GENERATED_BODY()
+
+	FHoudiniNode(){};
+	FHoudiniNode(int32 InNodeId);
+	
+	const HAPI_Session* Session {nullptr};
+	UPROPERTY(BlueprintReadWrite, meta=(ExposeOnSpawn))
+	int32 NodeId{-1};
+};
+
+UCLASS()
+class HOUDINIENGINE_API UHoudiniEngineBPExtension : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+	UFUNCTION(BlueprintPure, Category = "HoudiniEngine Extension", meta = (NativeMakeFunc, DisplayName = "Make Houdini Node"))
+	static FHoudiniNode MakeHoudiniNode_BP(int32 InNodeId) { return FHoudiniNode(InNodeId); }
+
+	template<typename TStruct>
+	static void SetArrayOfStructOnNode(const TArray<TStruct>& InArrayOfStruct, const FHoudiniNode& InNode, EAttributeOwner ImportLevel);
+	static void SetArrayOfStructOnNodeInternal(const FScriptArray& InArrayOfStruct, const FHoudiniNode& InNode, EAttributeOwner ImportLevel);
+
+	template<typename TStruct>
+	static void GetArrayOfStructOnNode(TArray<TStruct>& InArrayOfStruct, const FHoudiniNode& InNode, EAttributeOwner ImportLevel);
+	static void GetArrayOfStructOnNodeInternal(FScriptArray& InArrayOfStruct, const FHoudiniNode& InNode, EAttributeOwner ImportLevel);
+	
+	UFUNCTION(BlueprintCallable, CustomThunk, meta=(ArrayParm = "InArrayOfStruct"))
+	static void SetArrayOfStructOnNode_BP(const TArray<int32>& InArrayOfStruct, UPARAM(ref) const FHoudiniNode& InNode, EAttributeOwner ImportLevel);
+	//
+	DECLARE_FUNCTION(execSetArrayOfStructOnNode_BP);
+	
+};
+
+template <typename TStruct>
+void UHoudiniEngineBPExtension::SetArrayOfStructOnNode(const TArray<TStruct>& InArrayOfStruct,
+	const FHoudiniNode& InNode, EAttributeOwner ImportLevel)
+{
+}
+
+template <typename TStruct>
+void UHoudiniEngineBPExtension::GetArrayOfStructOnNode(TArray<TStruct>& InArrayOfStruct, const FHoudiniNode& InNode,
+	EAttributeOwner ImportLevel)
+{
+}
